aa8d573bb9e67eea4e28bd840588f8191313239e
grey423 <95908917+grey423@users.noreply.github.com>
2021-12-22 10:16:53 -0800

    Update and rename PrimeMega/modules/helper_funcs/chat_status.py to GreyUserbot/modules/helper-funcs/chat_status.py

--- GreyUserbot/modules/helper-funcs/chat_status.py
+++ GreyUserbot/modules/helper-funcs/chat_status.py
@@ -403,6 +403,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from PrimeMega.modules import connection
+from GreyCilik.modules import connection
 
 connected = connection.connected
