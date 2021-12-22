3fc098e2e00f8456f71554cafa431454db3544ed
grey423 <95908917+grey423@users.noreply.github.com>
2021-12-22 09:43:52 -0800

    Update mongo.py

--- PrimeMega/mongo.py
+++ PrimeMega/mongo.py
@@ -3,20 +3,20 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from PrimeMega import MONGO_DB_URI
-from PrimeMega.confing import get_int_key, get_str_key
+from GreyCilik import MONGO_DB_URI
+from GreyCilik.confing import get_int_key, get_str_key
 
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "PrimeMega"
+MONGO_DB = "GreyCilik"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["PrimeMega"]
+db = client["GreyCilik"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
