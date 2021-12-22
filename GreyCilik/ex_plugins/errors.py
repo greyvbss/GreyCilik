e468fb4b31340f3350bbe30c5fdb2e64f2112631
grey423 <95908917+grey423@users.noreply.github.com>
2021-12-22 10:06:50 -0800

    Update and rename PrimeMega/ex_plugins/errors.py to GreyCilik/ex_plugins/errors.py

--- GreyCilik/ex_plugins/errors.py
+++ GreyCilik/ex_plugins/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from PrimeMega import EVENT_LOGS, pbot as app
+from GreyCilik import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
