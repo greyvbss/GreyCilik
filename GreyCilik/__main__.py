b3e726888880010bff3fd7b210154278390bf25c
Grey Project <95908917+grey423@users.noreply.github.com>
2021-12-24 04:31:11 -0800

    Update __main__.py

--- GreyCilik/__main__.py
+++ GreyCilik/__main__.py
@@ -362,7 +362,7 @@ def grey_about_callback(update, context):
     query = update.callback_query
     if query.data == "grey_":
         query.message.edit_text(
-            text="๏ I'm *GreyCilik*, a powerful group management bot built to help you manage your group easily."
+            text="๏ I'm *Grey Cilik*, a powerful group management bot built to help you manage your group easily."
             "\n• I can restrict users."
             "\n• I can greet users with customizable welcome messages and even set a group's rules."
             "\n• I have an advanced anti-flood system."
