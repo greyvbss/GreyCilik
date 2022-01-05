1c0f61525f59d1a938dcd43f54e6dbb9759c6616
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-06 00:39:25 +0700

    Update alive.py

--- GreyCilik/modules/alive.py
+++ GreyCilik/modules/alive.py
@@ -15,7 +15,7 @@ PHOTO = "https://telegra.ph/file/760e7c0afaf6ba3df8ce7.jpg"
 async def awake(event):
   GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Grey Cilik.** \n\n"
   GREY += "⚪ **I'm Working Properly** \n\n"
-  GREY += f"⚪ **My Master : [Lord](https://t.me/reyyvbss)** \n\n"
+  GREY += f"⚪ **My Master : [Lord](https://t.me/greyyvbss)** \n\n"
   GREY += f"⚪ **Library Version :** `{telever}` \n\n"
   GREY += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   GREY += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
