9b1b8bb27305e29dfc54c898947c4e2e5b3ea78e
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-06 01:24:12 -0800

    Update __init__.py

--- GreyCilik/modules/sql/__init__.py
+++ GreyCilik/modules/sql/__init__.py
@@ -4,15 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from GreyCilik import DATABASE_URL, LOGGER
+from GreyCilik import DB_URL, LOGGER
 
 
 def start() -> scoped_session:
-    engine = create_engine(DATABASE_URL, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
     LOGGER.info("PostgreSQL Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+    return scoped_session(sessionmaker(bind=engine, autoflush=True))
 
 
 BASE = declarative_base()
