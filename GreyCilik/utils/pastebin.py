9c1946bc108fadfd527873d2f704a7df24738f3e
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-08 08:31:20 -0800

    Update pastebin.py

--- GreyCilik/utils/pastebin.py
+++ GreyCilik/utils/pastebin.py
@@ -1,5 +1,5 @@
 """
-MIT Licensee
+MIT License
 Copyright (c) 2021 TheHamkerCat
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
