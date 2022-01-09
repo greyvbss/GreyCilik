f2759152c89ff4617ed70551732aab86e868950d
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-09 06:10:45 -0800

    Update heroku.py

--- GreyCilik/modules/heroku.py
+++ GreyCilik/modules/heroku.py
@@ -193,7 +193,7 @@ async def _(dyno):
         dyno.chat_id,
         "logs.txt",
         reply_to=dyno.id,
-        caption="Prime logs.",
+        caption="Grey logs.",
     )
 
     await asyncio.sleep(5)
