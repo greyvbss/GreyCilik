6aa46e7c0883193365eb7ac11dc3992d782fe181
Grey Project <95908917+grey423@users.noreply.github.com>
2022-01-10 12:17:19 -0800

    Update forcesubs.py

--- GreyCilik/modules/forcesubs.py
+++ GreyCilik/modules/forcesubs.py
@@ -84,7 +84,7 @@ def _check_member(client, message):
             except UserNotParticipant:
                 try:
                     sent_message = message.reply_text(
-                        "Welcome {} 🙏 \n **Anda Belum Join Channel Kami @{} Channel yet**👷 \n \nPlease Joim [Our Channel](https://t.me/{}) dan Tekan Tombol **UNMUTE ME** Button. \n \n ".format(
+                        "Welcome {} 🙏 \n **You haven't joined our @{} Channel yet**👷 \n \nPlease Join [Our Channel](https://t.me/{}) and hit the **UNMUTE ME** Button. \n \n ".format(
                             message.from_user.mention, channel, channel
                         ),
                         disable_web_page_preview=True,
@@ -109,7 +109,7 @@ def _check_member(client, message):
                     )
                 except ChatAdminRequired:
                     sent_message.edit(
-                        "😕 **Grey is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
+                        "😕 **Prime is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
                     )
 
             except ChatAdminRequired:
