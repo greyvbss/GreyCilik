7cb3e2155b9472f41331987858164186c506fe9b
greyvbss <97778164+greyvbss@users.noreply.github.com>
2022-01-16 05:24:56 -0800

    Update __main__.py

--- GreyCilik/__main__.py
+++ GreyCilik/__main__.py
@@ -217,7 +217,7 @@ def start(update: Update, context: CallbackContext):
                         ],
                         [
                             InlineKeyboardButton(
-                                text=gs(chat.id, "help_button"), callback_data="help_back"),,
+                                text=gs(chat.id, "help_button"), callback_data="help_back"),
                         ]
                     ]
                 ),
